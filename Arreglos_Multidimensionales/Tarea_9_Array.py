d75f4dccdbb2e970ca47eaf6ee19aac9a508327f
kevinSalazarDev <k.salazar.ec@gmail.com>
2026-08-16 20:53:49 -0500

    3

--- Arreglos_Multidimensionales/Tarea_9_Array.py
+++ Arreglos_Multidimensionales/Tarea_9_Array.py
@@ -1,6 +1,6 @@
 numeros = [10, 20, 30, 40, 50]
 
-print("EL número que está en la posición 1 es:",numeros[1])
+print("El número que está en la posición 1 es:",numeros[1])
 
 for numero in numeros:
     print("El número es:", numero)
