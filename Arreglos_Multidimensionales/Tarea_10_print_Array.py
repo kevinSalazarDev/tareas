ec23d50fc7e0a100ed8fd39ffa17e0b170d130db
kevinSalazarDev <k.salazar.ec@gmail.com>
2026-08-23 22:05:31 -0500

    Agregar Tarea 10 print Array

---
+++ Arreglos_Multidimensionales/Tarea_10_print_Array.py
@@ -0,0 +1,14 @@
+matriz=[
+    [2, 4, 6],
+    [1, 3, 5],
+    [7, 8, 9]
+]
+
+print("La matriz es: ")
+
+for i in range (3):
+    for j in range (3):
+        print(matriz[i][j], end=" ")
+    print()
+
+
